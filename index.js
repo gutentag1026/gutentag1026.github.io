7547a0fde4f7572c18f936c4cb9e8379f79d84f5
Wendy Huang <huanghuang5087@hotmail.com>
2020-11-29 17:02:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ class Github {
   constructor() {
     this.client_id = 'd9308aacf8b204d361fd';
     this.client_secret = '84969aeef73956f4ec9e8716d1840532802bb81b';
-    this.repos_count = 5;
+    this.repos_count = 2;
     this.repos_sort = 'created: asc';
   }
 
@@ -147,4 +147,4 @@ searchUser.addEventListener('keyup', (e) => {
     // Clear profile
     ui.clearProfile();
   }
-});
+}); 
